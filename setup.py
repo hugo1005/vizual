d1f996d9d88010fe760ad70065381c5582f6fda1
hugo1005 <hugojdolan@gmail.com>
2021-12-31 16:30:05 +0000

    =v1

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
             ],
       },
       package_data = {
-            'static': ['*'],
+            'vizual/static': ['*'],
       },
       install_requires=['Flask','requests','docopt'], # add any additional packages that
       # needs to be installed along with your package. Eg: 'caer'
